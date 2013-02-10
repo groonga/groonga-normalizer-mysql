1a417b4d18a2d406cb57b565fa9136adff9b6dda
Kouhei Sutou <kou@clear-code.com>
2013-02-10 19:04:55 +0900

    Extract ctype-uca.c parser

--- tool/dump-difference-uca.rb
+++ tool/dump-difference-uca.rb
@@ -14,82 +14,19 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require "English"
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require "parser"
 
 if ARGV.size != 1
   puts("Usage: #{$0} MYSQL_SOURCE/strings/ctype-uca.c")
   exit(false)
 end
 
-def code_point_to_utf8(code_point)
-  [code_point].pack("U")
-end
-
-def utf8_to_code_point(utf8)
-  utf8.unpack("U")
-end
-
-current_page = nil
-pages = {}
-in_length = false
-lengths = []
-ARGF.each_line do |line|
-  case line
-  when / page(\d{3})data\[\]=/
-    current_page = $1.to_i(16)
-    pages[current_page] = []
-  when /^\s*0x(?:[\da-z]+)(?:,\s*0x(?:[\da-z]+))*,?$/i
-    next if current_page.nil?
-    weights = line.chomp.split(/,\s*/).collect do |component|
-      Integer(component)
-    end
-    pages[current_page].concat(weights)
-  when / uca_length\[256\]=/
-    in_length = true
-  when /^\d+(?:,\d+)*,?$/
-    next unless in_length
-    _lengths = line.chomp.split(/,/).collect {|length| Integer(length)}
-    lengths.concat(_lengths)
-  when /^\};$/
-    current_page = nil
-    in_length = false
-  end
-end
-
-pages.each do |page, flatten_weights|
-  weights = flatten_weights.each_slice(lengths[page])
-  pages[page] = weights.with_index.collect do |weight, i|
-    if weight.all?(&:zero?)
-      weight = [0]
-    else
-      while weight.last.zero?
-        weight.pop
-      end
-    end
-    code_point = (page << 8) + i
-    {
-      :weight     => weight,
-      :code_point => code_point,
-      :utf8       => code_point_to_utf8(code_point),
-    }
-  end
-end
-
-sorted_pages = pages.sort_by do |page, characters|
-  page
-end
-
-weight_based_characters = {}
-sorted_pages.each do |page, characters|
-  characters.each do |character|
-    weight = character[:weight]
-    weight_based_characters[weight] ||= []
-    weight_based_characters[weight] << character
-  end
-end
+parser = CTypeUCAParser.new
+parser.parse(ARGF)
 
 n_idencials = 0
-weight_based_characters.each do |weight, characters|
+parser.weight_based_characters.each do |weight, characters|
   next if characters.size == 1
   n_idencials += 1
   formatted_weight = weight.collect {|component| '%#07x' % component}.join(', ')
