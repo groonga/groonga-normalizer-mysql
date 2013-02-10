bfe2b53e99be3ea6dc3fe292cfc6e3aa58d83dbf
Kouhei Sutou <kou@clear-code.com>
2013-02-10 19:10:32 +0900

    Extract ctype-utf8.c parser

--- tool/parser.rb
+++ tool/parser.rb
@@ -27,6 +27,57 @@ module Unicode
   end
 end
 
+class CTypeUTF8Parser
+  def initialize
+    @planes = {}
+  end
+
+  def parse(input)
+    parse_ctype_utf8(input)
+    normalize_planes
+  end
+
+  def sorted_planes
+    @planes.sort_by do |plane, characters|
+      plane
+    end
+  end
+
+  private
+  def parse_ctype_utf8(input)
+    current_plane = nil
+    input.each_line do |line|
+      case line
+      when / plane(\d{2})\[\]=/
+        current_plane = $1.to_i(16)
+        @planes[current_plane] = []
+      when /^\s*
+             \{0x([\da-z]+),0x([\da-z]+),0x([\da-z]+)\},
+             \s*
+             \{0x([\da-z]+),0x([\da-z]+),0x([\da-z]+)\},?$/ix
+        next if current_plane.nil?
+        parsed_characters = $LAST_MATCH_INFO.captures.collect do |value|
+          Unicode.to_utf8(value.to_i(16))
+        end
+        upper1, lower1, sort1, upper2, lower2, sort2 = parsed_characters
+        characters = @planes[current_plane]
+        characters << {:upper => upper1, :lower => lower1, :sort => sort1}
+        characters << {:upper => upper2, :lower => lower2, :sort => sort2}
+      when /^\};$/
+        current_plane = nil
+      end
+    end
+  end
+
+  def normalize_planes
+    @planes.each do |plane, characters|
+      characters.each_with_index do |character, i|
+        character[:base] = Unicode.to_utf8((plane << 8) + i)
+      end
+    end
+  end
+end
+
 class CTypeUCAParser
   attr_reader :pages
   def initialize
